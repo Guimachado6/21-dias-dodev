634e171469bfb6694a7acaec4e29400776e61693
Guilherme Machado <guilherme03_df@hotmail.com>
2023-09-20 21:39:39 -0300

    Dia 5 Switch

---
+++ dia 5/index.js
@@ -0,0 +1,92 @@
+/*var opcao = prompt("Escolha uma opção de 1 a 5")
+
+switch (opcao){
+    case "1":
+        console.log("voce escolheu a opção 1")
+    break;
+    case "2":
+        console.log("voce escolheu a opção 2")
+    break;
+    case "3":
+        console.log("voce escolheu a opção 3")
+    break;
+    case "4":
+        console.log("voce escolheu a oção 4")
+    break;
+    case "5":
+        console.log("voce escolhe a opção 5")
+    break;
+    default:
+        console.log("opção inválida")
+        break;
+    
+}*/
+
+/*
+let idade = Number(prompt("Insira sua idade"))
+
+switch (idade) {
+    case 10:
+        console.log("voce tem 10 anos")
+        break;
+    case 15:
+        console.log("voce tem 15 anos")
+        break;
+    default:
+        console.log("opção inválida")
+        break;
+}
+*/
+
+/*
+let number1
+let number2
+
+number1 = Number(prompt("Escolha o primeiro número"))
+number2 = Number(prompt("Escolha o segundo número"))
+
+let operacao = prompt("Escolha de 1 a 4 para selecionar qual operação será feita : 1: adição; 2: subtração; 3: multiplicação; 4: divisão.")
+
+switch(operacao){
+    case "1":
+        console.log(number1 + number2)
+    break;
+    case "2":
+        console.log(number1 - number2)
+    break;
+    case "3":
+        console.log(number1 * number2)
+    break;
+    case "4":
+        console.log(number1 / number2)
+    break;
+    default:
+        console.log("Operação Inválida")
+    break;
+
+}
+*/
+/*
+let servico = prompt("Qual será o serviço desejado? escolha opções de 1 a 3; 1:Gasolina; 2:Alcool; 3:Calibrar os pneus.")
+let gasolina
+let alcool
+let calibrar
+
+switch(servico){
+    case "1":
+        litros = Number(prompt("Qual o valor que será abastecido em ?"))
+        gasolina = 5
+        console.log(litros / gasolina + " Litros")
+    break;
+    case "2":
+        litros = Number(prompt("Qual o valor que será abastecido em ?"))
+        alcool = 3
+        console.log(litros / alcool + " Litros")
+    break;
+    case "3":
+        console.log("Pneus calibrados com sucesso")
+    break;
+    default:
+        console.log("Operação Inválida")
+    break;
+}*/
